e81693057cae028e407f27128ca0242b2b2d2eff
Arguello Soledad <soledad.ar1@gmail.com>
2023-07-04 21:06:56 -0300

    ciclo de vida de un componente

--- src/app/listado/listado.component.ts
+++ src/app/listado/listado.component.ts
@@ -13,6 +13,27 @@ export class ListadoComponent {
   @Output()
   borrarPais = new EventEmitter()
 
+
+  // Ciclos de vida 
+  /*Al construirse inicia el array vacio luego de construido se ejecutan 
+  los Input y Output y recien ahi se le asigna valor si es que lo tiene a
+  la variable osea se inicializa */
+  constructor(){
+    console.log('Constructor del listado', this.paises.length)
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit del listado', this.paises.length)
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit del listado', this.paises.length)
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy del listado', this.paises.length)
+  }
+
   removePais(paisABorrar:String){
     this.borrarPais.emit(paisABorrar)
     //this.paises = this.paises.filter(pais => pais !== paisABorrar)
