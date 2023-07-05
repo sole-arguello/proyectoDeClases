ca0ec83c59b897839dcabdf920ede3f43b08a22c
Arguello Soledad <soledad.ar1@gmail.com>
2023-07-05 19:27:24 -0300

    clase 2 redireccionamiento por errores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
     component: ProfilePageComponent
   },
   {
+    path: 'profil',
+    redirectTo: 'profile'
+  },
+  {
     path: 'checkout',
     component: CheckoutPageComponent
   }
