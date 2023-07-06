62af15248d3e6fa5c0ecbc3739d443af0999423e
Arguello Soledad <soledad.ar1@gmail.com>
2023-07-05 21:17:56 -0300

    del 1 envio al 3 pasando por el 2 info, con property binding

---
+++ clase3/proyectoClase3/src/app/component2/component2.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component {
+
+  @Input()
+  nombre: string = ''
+
+}
---
+++ clase3/proyectoClase3/src/app/component3/component3.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-component3',
+  templateUrl: './component3.component.html',
+  styleUrls: ['./component3.component.css']
+})
+export class Component3Component {
+  @Input()
+  nombre: string = ''
+}
